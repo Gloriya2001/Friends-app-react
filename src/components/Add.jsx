71062505d147545455918557a179594357e02fe3
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-10 12:31:35 +0530

    form value collected

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const Add = () => {
 
+    const [data, setData] = useState(
+
+        {
+            "name": "",
+            "friendName": "",
+            "friendNickName": "",
+            "DescribeYourFriend": ""
+        }
+
+
+    )
+    const inputHandler = (event) => {
+        setData({ ...data, [event.target.name]: event.target.value })
+    }
+
+    const readValue = () => {
+        console.log(data)
+    }
 
 
 
-    
     return (
         <div>
 
-<Navbar/>
+            <Navbar />
 
 
 
@@ -23,35 +40,34 @@ const Add = () => {
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                                 <label htmlFor="" className="form-label">Name</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name='name' value={data.name} onChange={inputHandler} />
 
                             </div>
 
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                                 <label htmlFor="" className="form-label">Friend Name</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name='friendName' value={data.friendName} onChange={inputHandler} />
 
                             </div>
 
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                                 <label htmlFor="" className="form-label">Friend Nick Name</label>
-                                <input type="
-                                " className="form-control" />
+                                <input type="" className="form-control" name='friendNickName' value={data.friendNickName} onChange={inputHandler}/>
                             </div>
 
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                                 <label htmlFor="" className="form-label">Describe your friend</label>
-                                <input type="text" className="form-control" />
+                                <input type="text" className="form-control" name='DescribeYourFriend' value={data.DescribeYourFriend} onChange={inputHandler}/>
 
                             </div>
 
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
-                                <button className="btn btn-success">Add Friend</button>
-                         
+                                <button className="btn btn-success" onClick={readValue}>Add Friend</button>
+
                             </div>
                         </div>
 
