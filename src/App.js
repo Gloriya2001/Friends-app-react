165544b5b988356022603dde3b9b9f2a8a76eca7
Gloriya2001 <glooriaa2001@gmail.com>
2024-06-10 11:53:01 +0530

    view and navbar set

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Add from './components/Add';
+import View from './components/View';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <Add/>
+      <View/>
       
     </div>
   );
---
+++ src/components/View.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const View = () => {
+    return (
+        <div>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Friend Name</th>
+                                    <th scope="col">Friend's Nick Name</th>
+                                    <th scope="col">Describe your Friend</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+
+                            </tbody>
+                        </table>
+
+
+
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default View
